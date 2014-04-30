73ce08d4ac51a8aadbd753e2edd4ac20aa575353
INKONIQ <INKONIQ@INKONIQ-PC>
2014-04-30 23:08:55 +0530

    dashboard UI fix

--- protected/modules/dashboard/views/default/index.php
+++ protected/modules/dashboard/views/default/index.php
@@ -40,49 +40,58 @@
 }
 </style>
 <h1 class="dash-header">Home</h1>
-<div class="container home_dashboard">
-	<div class="row"><a href="<?php echo Yii::app()->createUrl("/dashboard/reporting") ?>" >
-                      <button href="<?php echo Yii::app()->createUrl("/dashboard/reporting") ?>"  class="btn btn-xlarge" /><i class="fa fa-bar-chart-o fa-5x" ></i><br/> 
-                      <h2>REPORTING</h2>
-                      <p>An overview 
-of Stereotribes 
- </p>        
-                    </button></a>
-                     <a href="<?php echo Yii::app()->createUrl("/dashboard/users") ?>"   /><button href="<?php echo Yii::app()->createUrl("/dashboard/users") ?>"  class="btn btn-xlarge" /><i class="fa fa-group fa-5x" ></i><br/> 
-                      <h2>USERS 
-</h2>
-                      <p>Our funders, 
-creaters & lovers 
- </p>        
-                    </button></a>
-                                 <a href="<?php echo Yii::app()->createUrl("/dashboard/projects") ?>"   /><button href="<?php echo Yii::app()->createUrl("/dashboard/projects") ?>"  class="btn btn-xlarge" /><i class="fa fa-bookmark fa-5x" ></i><br/> 
-                      <h2>PROJECTS 
-</h2>
-                      <p>All ‘Play’ 
-    campaign details 
- </p>        
-                    </button></a>
-                                <a href="<?php echo Yii::app()->createUrl("/dashboard/categories") ?>"  class="" /> <button href="<?php echo Yii::app()->createUrl("/dashboard/categories") ?>"  class="btn btn-xlarge" /><i class="fa fa-bookmark fa-5x" ></i><br/> 
-                      <h2>CATEGORIES
-</h2>
-                      <p>Our category
-engagement
- </p>        
-                    </button></a>
-                               <a href="<?php echo Yii::app()->createUrl("/dashboard/financial") ?>"  class="" />  <button href="<?php echo Yii::app()->createUrl("/dashboard/financial") ?>"  class="btn btn-xlarge" /><i class="fa fa-bars fa-5x" ></i><br/> 
-                      <h2>FINANCIAL 
-</h2>
-                      <p>Stereotribes 
-financial overview 
- </p>        
-                    </button></a>
-                                <a href="<?php echo Yii::app()->createUrl("/dashboard/tribes") ?>"  class="" /> <button href="<?php echo Yii::app()->createUrl("/dashboard/tribes") ?>"  class="btn btn-xlarge" /><i class="fa fa-sitemap fa-5x" ></i><br/> 
-                      <h2>TRIBES 
-</h2>
-                      <p>Group hubs, and 
-group comms 
- </p>        
-                    </button></a>
+  
+  <div class="home_dashboard">
+	 
+   <div class="row">
+
+      <a class="col-md-4" href="<?php echo Yii::app()->createUrl("/dashboard/reporting") ?>">
+        <button href="<?php echo Yii::app()->createUrl("/dashboard/reporting") ?>" class="btn btn-xlarge">
+          <i class="fa fa-bar-chart-o fa-5x" ></i><br/> 
+          <h2>REPORTING</h2>
+          <p>An overview of Stereotribes</p>        
+        </button>
+      </a>
+        
+      <a class="col-md-4" href="<?php echo Yii::app()->createUrl("/dashboard/users") ?>">
+        <button href="<?php echo Yii::app()->createUrl("/dashboard/users") ?>"  class="btn btn-xlarge">
+          <i class="fa fa-group fa-5x" ></i><br/> 
+          <h2>USERS</h2>
+          <p>Our funders, creaters & lovers</p>        
+        </button>
+      </a>
+       
+      <a class="col-md-4" href="<?php echo Yii::app()->createUrl("/dashboard/projects") ?>">
+        <button href="<?php echo Yii::app()->createUrl("/dashboard/projects") ?>" class="btn btn-xlarge">
+          <i class="fa fa-bookmark fa-5x" ></i><br/> 
+          <h2>PROJECTS</h2>
+          <p>All ‘Play’ campaign details</p>        
+        </button>
+      </a>
+      
+      <a class="col-md-4" href="<?php echo Yii::app()->createUrl("/dashboard/categories") ?>">
+        <button href="<?php echo Yii::app()->createUrl("/dashboard/categories") ?>" class="btn btn-xlarge">
+          <i class="fa fa-bookmark fa-5x" ></i><br/> 
+          <h2>CATEGORIES</h2>
+          <p>Our categor engagement</p>        
+        </button>
+      </a>
+      
+      <a class="col-md-4" href="<?php echo Yii::app()->createUrl("/dashboard/financial") ?>">
+        <button href="<?php echo Yii::app()->createUrl("/dashboard/financial") ?>" class="btn btn-xlarge">
+          <i class="fa fa-bars fa-5x" ></i><br/> 
+          <h2>FINANCIAL</h2>
+          <p>Stereotribes financial overview</p>        
+        </button>
+      </a>
+      
+      <a class="col-md-4" href="<?php echo Yii::app()->createUrl("/dashboard/tribes") ?>">
+        <button href="<?php echo Yii::app()->createUrl("/dashboard/tribes") ?>" class="btn btn-xlarge">
+          <i class="fa fa-sitemap fa-5x" ></i><br/> 
+          <h2>TRIBES</h2>
+          <p>Group hubs, and group comms</p>        
+        </button>
+      </a>
 <!--                                 <button href="#"  class="btn btn-xlarge" /><i class="fa fa-bookmark fa-5x" ></i><br/> 
                       <h2>SUPPORT 
 </h2>
@@ -99,13 +108,13 @@ analytics,
 descriptions
  </p>        
                     </button>-->
-            </button>
-                                 <button href="#"  class="btn btn-xlarge" /><i class="fa fa-cog fa-5x" ></i><br/> 
-                      <h2>SETTINGS
-</h2>
-                      <p>Admin settings
- </p>        
-                    </button>
+      <a class="col-md-4" href="#">
+        <button href="#" class="btn btn-xlarge">
+          <i class="fa fa-cog fa-5x" ></i><br/> 
+          <h2>SETTINGS</h2>
+          <p>Admin settings</p>        
+        </button>
+      </a>
               
     </div>
 </div>
