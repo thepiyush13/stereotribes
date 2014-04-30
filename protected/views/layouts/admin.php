37c6a764837c47313266ff1ace2626e1af2d4430
INKONIQ <INKONIQ@INKONIQ-PC>
2014-04-30 15:33:02 +0530

    dashboard UI fix

--- protected/views/layouts/admin.php
+++ protected/views/layouts/admin.php
@@ -40,7 +40,7 @@
     <body>
         <section id="container" class="">
             <!--header start-->
-            <header class="header white-bg"> <div class="sidebar-toggle-box">
+            <header class="header stereo-bg"> <div class="sidebar-toggle-box">
                     <div data-original-title="Toggle Navigation" data-placement="right" class="icon-reorder tooltips"></div>
                 </div>
                 <!--logo start-->
