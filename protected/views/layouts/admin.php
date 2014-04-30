7414e2f0c98daea9685977776e40cd23d5932221
INKONIQ <INKONIQ@INKONIQ-PC>
2014-04-30 17:36:54 +0530

    dashboard UI fix

--- protected/views/layouts/admin.php
+++ protected/views/layouts/admin.php
@@ -47,12 +47,12 @@
                 <a href="/admin" class="logo" >STEREOTRIBES</a>
                 <!--logo end-->
                 <div class="top-nav pull-right">
-                    <ul class="nav top-menu pull-left">
+                    <ul class="nav top-menu pull-left search-menu">
                         <li>
                             <input type="text" class="form-control" placeholder="Search">
                         </li>
                     </ul>
-                    <div class="pull-right"> 
+                    <div class="pull-right log"> 
                         <!-- user login dropdown start-->
                         <?php $this->widget('application.components.LoginBar');?>
                         <!-- user login dropdown end -->  
