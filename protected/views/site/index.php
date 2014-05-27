e9cb31539031844426d3b0c4a30ab60a4758c975
INKONIQ <INKONIQ@INKONIQ-PC>
2014-05-27 10:40:59 +0530

    New Home Page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,21 +1,25 @@
-<!-- ==================== Video Section ====================== -->
+==================== Video Section ====================== -->
 
-	 <?php  
-    $theme_home_folder = Yii::app()->baseUrl;
+	<?php  
+    	$theme_home_folder = Yii::app()->baseUrl;
     ?>
-    <video id="bgvid" autoplay loop preload="none" poster="<?php echo $theme_home_folder; ?>img/video-poster.jpg" data-setup="{}">
-	  <source src="<?php echo $theme_home_folder; ?>videos/st.webm" type="video/webm">
-	  <source src="<?php echo $theme_home_folder; ?>videos/st.mp4" type="video/mp4">
-      <source src="<?php echo $theme_home_folder; ?>videos/st.ogv" type="video/ogg">
-      <source src="<?php echo $theme_home_folder; ?>videos/st.ogg" type="video/ogg">
-      <source src="<?php echo $theme_home_folder; ?>videos/st.mov" type="video/mov">
-	</video>
+
+    <div class="vidwrap">
+	    <video id="bgvid" autoplay loop preload="none" poster="<?php echo $theme_home_folder; ?>img/video-poster.jpg" data-setup="{}">
+		  <source src="<?php echo $theme_home_folder; ?>videos/st.webm" type="video/webm">
+		  <source src="<?php echo $theme_home_folder; ?>videos/st.mp4" type="video/mp4">
+	      <source src="<?php echo $theme_home_folder; ?>videos/st.ogv" type="video/ogg">
+	      <source src="<?php echo $theme_home_folder; ?>videos/st.ogg" type="video/ogg">
+	      <source src="<?php echo $theme_home_folder; ?>videos/st.mov" type="video/mov">
+		</video>
+		<i class="icon-arrow-down7"></i>
+	</div>
 
 	<div class="main-wrapper">
 
 		<!-- ==================== Secondary Navigation ====================== -->
 
-	    <nav id="secondary-nav" class="secondary-nav" role="navigation">
+	    <!-- <nav id="secondary-nav" class="secondary-nav" role="navigation">
 
 	    	<div class="container">
 
@@ -34,25 +38,404 @@
 
 			</div>
 
-		</nav>
+		</nav> -->
+
+		<section class="amplify-passion-section">
+			
+			<div class="container">
+
+				<h1 class="amplify-passion-heading">FIND YOUR TRIBE</h1>
+
+				<p class="amplify-passion-subheading">FIND &amp; FUND PASSIOTNATE MUSIC PIONEERS  <a href="">VIEW MORE TRIBES<i class="icon-arrow-right6"></i></a></p>
+
+			</div>
+
+		</section>
 
 		<!-- ==================== Site Contents Section ====================== -->
 
-		<div class="freewall" id="main_grid_content">
-
+		<div class="freewall" id="main_grid_content"></div>                
 		
-
-		</div>
-<!--                <div class='text-center'>
-                    <input type="button" class="btn btn-large btn-default" id='load_more_blocks' value='Load More'/>
-                    <input type="hidden" id='page_number' value='1'/>
-                </div>-->
-                
-		
-    <div class="container text-center">
+    	<!-- <div class="container text-center">
 			<button class="add-more" id='load_more_blocks' >Load More</button>
-                         <input type="hidden" id='page_number' value='1'/>
-		</div> 
+            <input type="hidden" id='page_number' value='1'/>
+		</div>  -->
+
+		<div class="container text-center">
+			<button class="add-more">View More<i class="icon-arrow-right6"></i></button>
+		</div>
+
+
+		<section id="stereosection-1" class="stereosection" data-speed="10" data-type="background"></section>
+
+		<section class="amplify-passion-section">
+			
+			<div class="container">
+
+				<h1 class="amplify-passion-heading">AMPLIFY YOUR PASSION</h1>
+
+				<p class="amplify-passion-subheading">WE’RE PASSIONATE MUSIC LOVERS, PROUD TO BE CREATIVE  <a href="">EXPLORE OUR STORIES<i class="icon-arrow-right6"></i></a></p>
+			
+			</div>
+
+		</section>
+
+		<iframe src="//player.vimeo.com/video/75164621" class="passion-video" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> 
+
+		<section id="stereosection-2" class="stereosection" data-speed="10" data-type="background"></section>
+
+		<section class="amplify-passion-section">
+			
+			<div class="container">
+
+				<h1 class="amplify-passion-heading">WE UNDERSTAND YOU</h1>
+
+				<p class="amplify-passion-subheading">OUR TRIBE IS MADE UP OF MUSICIANS &amp; MUSIC LOVERS  <a href="">ABOUT STEREOTRIBES<i class="icon-arrow-right6"></i></a></p>
+
+			</div>
+
+		</section>
+
+		<section class="we-tribes-section" data-speed="50" data-type="background">
+			
+			<div class="container">
+				
+				<div class="row">
+					
+					<div class="col-md-5 stereo-container-1">
+						
+						<div class="stereo-text-container">
+							<img src="img/stereo-text-1.png" class="img-responsive" />
+						</div>
+
+						<p class="stereo-text-para">We love being part of the 
+						community and listening to the voice of creatives. We’re made 
+						up of passionate music lovers 
+						innovating new ways to showcase creativity through ‘Tribefunding’.</p>
+
+					</div>
+
+					<div class="col-md-7 stereo-container-2">
+						
+						<div class="stereo-text-container">
+							<img src="img/stereo-text-2.png" class="img-responsive" />
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</section>
+
+		<section id="stereosection-4" class="stereosection" data-speed="20" data-type="background"></section>
+
+		<section class="amplify-passion-section">
+			
+			<div class="container">
+
+				<h1 class="amplify-passion-heading">WE OFFER YOU MORE</h1>
+
+				<p class="amplify-passion-subheading">WE OFFER YOU MORE SUPPORT, SO YOU CAN FOCUS ON YOUR PASSION  <a href="">HOW WE HELP<i class="icon-arrow-right6"></i></a></p>
+
+			</div>
+
+		</section>
+
+		<section class="weoffermore-section" data-speed="50" data-type="background">
+			
+			<div class="container">
+				
+				<div class="row">
+					
+					<div class="col-md-3 offer-links-wrap">
+						<a href="" class="offer-links">CAMPAIGN<br> FEATURING</a>
+					</div>
+
+					<div class="col-md-3 offer-links-wrap">
+						<a href="" class="offer-links">MARKETING<br> SUPPORT</a>
+					</div>
+
+					<div class="col-md-3 offer-links-wrap">
+						<a href="" class="offer-links">PITCH VIDEO<br> SUPPORT</a>
+					</div>
+
+					<div class="col-md-3 offer-links-wrap">
+						<a href="" class="offer-links">SOCIAL<br> DASHBOARD</a>
+					</div>
+
+					<div class="col-md-3 offer-links-wrap">
+						<a href="" class="offer-links">SEARCH BY<br> REWARDS</a>
+					</div>
+
+					<div class="col-md-3 offer-links-wrap">
+						<a href="" class="offer-links">PRIZES &amp; VIP<br> EVENTS</a>
+					</div>
+
+					<div class="col-md-3 offer-links-wrap">
+						<a href="" class="offer-links">LOCAL &amp; TRIBE<br> FOCUSED</a>
+					</div>
+
+					<div class="col-md-3 offer-links-wrap">
+						<a href="" class="offer-links">FREE <br>WORKSHOPS</a>
+					</div>
+
+				</div>
+
+			</div>
+
+		</section>
+
+
+		<section id="stereosection-6" class="stereosection" data-speed="10" data-type="background"></section>
+
+		<section class="amplify-passion-section">
+			
+			<div class="container">
+
+				<h1 class="amplify-passion-heading">LET’S GET STARTED</h1>
+
+				<p class="amplify-passion-subheading">ARE YOU A CREATOR, FUNDER OR SUPPORTOR OF MUSIC CAMPAIGNS?</p>
+
+			</div>
+
+		</section>
+
+
+		<section class="get-started-section" data-speed="50" data-type="background">
+			
+			<div class="container">
+				
+				<div class="col-md-4 get-started-block">
+					
+					<a href="" class="get-start-circle">Play</a>
+
+					<h3 class="get-start-head">CREATE A <br>CAMPAIGN</h3>
+
+					<a href="" class="get-started-link">Learn to create campaigns</a>
+
+				</div>
+
+				<div class="col-md-4 get-started-block">
+					
+					<a href="" class="get-start-circle">Find</a>
+
+					<h3 class="get-start-head">DISCOVER<br> &amp; FUND</h3>
+
+					<a href="" class="get-started-link">Fund tribes to win prizes</a>
+
+				</div>
+
+				<div class="col-md-4 get-started-block">
+					
+					<a href="" class="get-start-circle">Love</a>
+
+					<h3 class="get-start-head">FOLLOW<br> &amp; SHARE</h3>
+
+					<a href="" class="get-started-link">Hang out with cool tribes</a>
+
+				</div>
+
+			</div>
+
+		</section>
+
+
+		<!-- Footer Area -->
+    
+    <footer class="footer-main-wrapper" data-speed="10" data-type="background">
+
+        <div class="container">
+
+            <section class="footer-section">
+            
+                <div class="col-md-6 footer-left">
+
+                    <div class="footer-container footer-container-height">
+                    
+                        <div class="footer-blocks">
+
+                            <h4 class="footer-heading">FOR THE <br>FUNDERS</h4>
+                            <ul class="footer-lists">
+                                <li><a href="#">Why contribute?</a></li>
+                                <li><a href="#">Fixed vs flexible</a></li>
+                                <li><a href="#">Funder competition</a></li>
+                                <li><a href="#">Passion is our game</a></li>
+                            </ul>
+
+                        </div>
+
+                        <div class="footer-blocks">
+
+                            <h4 class="footer-heading">FOR THE<br> CAMPAIGNERS</h4>
+                            <ul class="footer-lists">
+                                <li><a href="#">Do I use Stereotribes?</a></li>
+                                <li><a href="#">Rehearsal room</a></li>
+                                <li><a href="#">Marketing support</a></li>
+                                <li><a href="#">Pitch video packages</a></li>
+                            </ul>
+
+                        </div>
+
+                        <div class="footer-blocks">
+
+                            <h4 class="footer-heading">FOR THE <br>LOVERS</h4>
+                            <ul class="footer-lists">
+                                <li><a href="#">Tribe passion</a></li>
+                                <li><a href="#">Never too young</a></li>
+                                <li><a href="#">Join our tribe today</a></li>
+                            </ul>
+
+                        </div>
+
+                    </div>
+
+                </div><!-- Footer Left ends -->
+
+                <div class="col-md-6 footer-right">
+
+                    <div class="footer-container footer-container-height">
+                    
+                        <div class="footer-blocks">
+
+                            <h4 class="footer-heading">WE ARE<br> STEREOTRIBES</h4>
+                            <ul class="footer-lists">
+                                <li><a href="#">The Stereotribes story</a></li>
+                                <li><a href="#">How we’re different</a></li>
+                                <li><a href="#">Tribe jobs</a></li>
+                                <li><a href="#">In the press</a></li>
+                                <li><a href="#">Rockin’ projects</a></li>
+                                <li><a href="#">Media enquiries</a></li>
+                            </ul>
+
+                        </div>
+
+                        <div class="footer-blocks">
+
+                            <h4 class="footer-heading">OUR<br> SUPPORT</h4>
+                            <ul class="footer-lists">
+                                <li><a href="#">FAQ’S</a></li>
+                                <li><a href="#">Contact us</a></li>
+                                <li><a href="#">Live chat 24x7</a></li>
+                            </ul>
+
+                        </div>
+
+                        <div class="footer-blocks">
+
+                            <h4 class="footer-heading">SOCIAL<br> IS SEXY</h4>
+                            <ul class="footer-lists">
+                                <li><a href="#" class="footer-social footer-facebook">Facebook</a></li>
+                                <li><a href="#" class="footer-social footer-twitter">Twitter</a></li>
+                                <li><a href="#" class="footer-social footer-instagram">Instagram</a></li>
+                                <li><a href="#" class="footer-social footer-gplus">Google +</a></li>
+                                <li><a href="#" class="footer-social footer-vimeo">Vimeo</a></li>
+                                <li><a href="#" class="footer-social footer-youtube">Youtube</a></li>
+                            </ul>
+
+                        </div>
+
+                    </div>
+
+                </div><!-- Footer Right ends -->
+
+                <div class="col-md-6 footer-left">
+
+                    <div class="footer-container">
+
+                        <div class="footer-blocks">
+                        
+                            <h4 class="footer-heading">THE WORLD SAYS...</h4>
+
+                            <div id="owl-demo" class="owl-carousel">
+              
+                              <div class="item"><h4 class="testimonial-header">“Stereotribes is breaking new ground in the crowdfunding World.” - Mashable</h4></div>
+
+                              <div class="item"><h4 class="testimonial-header">“Stereotribes is breaking new ground in the crowdfunding World.” - Mashable</h4></div>
+
+                              <div class="item"><h4 class="testimonial-header">“Stereotribes is breaking new ground in the crowdfunding World.” - Mashable</h4></div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div><!-- Footer Left ends -->
+
+                <div class="col-md-6 footer-right">
+
+                    <div class="footer-container">
+
+                        <div class="footer-blocks">
+                            
+                            <h4 class="footer-heading">STEREOTRIBES<br> BLOG</h4>
+
+                            <div class="blog-img">
+                                <a href="#"><img src="img/blog.png" class="img-responsive" /></a>
+                            </div>
+
+                        </div>
+
+                        <div class="footer-blocks">
+                            
+                            <h4 class="footer-heading">VIP E-NEWS SIGN UP</h4>
+
+                            <div class="form-group footer-textbox">
+                                <input type="email" class="form-control" placeholder="Type email...">
+                                <button type="submit" class="btn">
+                                  <i class="icon-arrow-right6"></i>
+                                </button>
+                            </div>
+
+                        </div>
+                        
+                    </div>
+
+                </div><!-- Footer Right ends -->
+
+            </section>
+
+        </div>
+
+        <section class="footer-copyright">
+            
+            <div class="container">
+                
+                <div class="row">
+                    
+                    <div class="col-md-6">
+                        
+                        <nav class="privacy-links">
+                            
+                            <a href="#">Terms of Use</a>
+                            <a href="#">Privacy Policy</a>
+                            <a href="#">Cookie Policy</a>
+
+                        </nav>
+
+                    </div>
+
+                    <div class="col-md-6">                  
+
+                        <p class="copyright text-right">© 2014 Stereotribes, Inc. All rights reserved</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </footer>
+
+    <!-- Footer Area -->
+
+
+
+		<!-- Modal Box -->
 
 		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 		  <div class="modal-dialog">
